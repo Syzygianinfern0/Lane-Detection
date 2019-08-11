0825c43ed1d2d8d49a5cc1e1d5ccca4af100069f
Syzygianinfern0 <spsharan2000@gmail.com>
2019-08-11 19:50:07 +0530

    Region of Interest

--- detect.py
+++ detect.py
@@ -5,13 +5,31 @@ import matplotlib.pyplot as plt
 VIDEO = 'assets/dashcamvid.mp4'
 PIC = 'assets/dashcampic.png'
 
+
+def roi(image):
+    top_right = [830, 400]
+    top_left = [630, 400]
+    bottom_left = [350, 600]
+    bottom_right = [1050, 600]
+    vertices = [np.array([bottom_left, bottom_right, top_right, top_left], dtype=np.int32)]
+
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, vertices, 255)
+
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
 if __name__ == '__main__':
     test_img = cv2.imread(PIC, cv2.IMREAD_GRAYSCALE)
     blur_image = cv2.GaussianBlur(test_img, (3, 3), 0)
     canny_image = cv2.Canny(blur_image, 100, 150)
+
+    roi_img = roi(canny_image)
+
     cv2.imshow('test', test_img)
     cv2.imshow('blur_image', blur_image)
     cv2.imshow('canny_image', canny_image)
+    cv2.imshow('roi_img', roi_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
